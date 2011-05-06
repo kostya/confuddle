b9bc5c128cf48b511854ca0be6e66a8fd4f6ccd9
Konstantin Makarchev <kostya27@gmail.com>
2011-05-06 23:58:44 +0400

    refactor peoples

--- lib/unfuzzle/lib/unfuzzle/person.rb
+++ lib/unfuzzle/lib/unfuzzle/person.rb
@@ -6,6 +6,11 @@ module Unfuzzle
     attribute :last_name, :from => 'last-name'
     attribute :username, :from => 'username'
 
+    def self.all
+      response = Request.get("/people")
+      collection_from(response.body, 'people/person')
+    end
+
     def self.all_for_project(project_id)
       response = Request.get("/projects/#{project_id}/people")
       collection_from(response.body, 'people/person')
